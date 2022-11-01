aaeefd4718206a0c708e3bff9b3c1400ebb3d27e
masudcse100 <masudcse74@gmail.com>
2022-11-01 15:50:18 +0600

    Arrow Function

--- arrow.js
+++ arrow.js
@@ -9,14 +9,17 @@
 const doubleIt = num=> num * 3;
 const result = doubleIt(17);
 console.log(result);
+
 // ----------------------------------
 const addsum = (x,y) => x+y;
 const result2 = addsum(54,34);
 console.log(result2);
+
 // ----------------------------------
 const give5 = () =>5;
 const result3 = give5();
 console.log(result3);
+
 // ----------------------------------
 const doMath = (x,y) => {
   const sum = x+y;
