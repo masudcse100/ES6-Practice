cd793e5f0f87c48902aaac15d973ff3cbe2fbfd5
masudcse100 <masudcse74@gmail.com>
2022-11-01 15:52:52 +0600

    Template JS

--- template.js
+++ template.js
@@ -1,13 +1,15 @@
 const firstName = "Justin";
 const lastName = "TimberLake";
 const fullName = firstName +" "+ lastName;
+
 const fullName2 = `${firstName} ${lastName} ${5*3+10} is a good boy.`; // Es6 template is powerfull
 console.log("User full Name: "+fullName);
 console.log(fullName2);
+
 const multiLine = "I love you\n"
   + "i miss you\n"
   + "I need you"; //old multiLine system
-  
+
 // Es6 template is powerfull
 const multiLine2 = `I love you
 I miss you
